fd5efe58526b7840b1f0138a632ddfed8c09261f
Jan <stueven@uni-bremen.de>
2020-01-09 17:39:08 +0100

    Buttons eingefügt

--- Anselm/app/src/main/java/ml/meiner/anselm/Activties/MapActivity.java
+++ Anselm/app/src/main/java/ml/meiner/anselm/Activties/MapActivity.java
@@ -1,8 +1,12 @@
 package ml.meiner.anselm.Activties;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import ml.meiner.anselm.Main.MainActivity;
 import ml.meiner.anselm.R;
 
 public class MapActivity extends AppCompatActivity {
@@ -11,5 +15,11 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapactivity);
+    }
+
+    public void backtoMain(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- Anselm/app/src/main/java/ml/meiner/anselm/Main/MainActivity.java
+++ Anselm/app/src/main/java/ml/meiner/anselm/Main/MainActivity.java
@@ -1,11 +1,15 @@
 package ml.meiner.anselm.Main;
 
 import android.content.Intent;
+import android.media.Rating;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import ml.meiner.anselm.Activties.HistoryActivity;
 import ml.meiner.anselm.Activties.MapActivity;
+import ml.meiner.anselm.Activties.RatingActivity;
 import ml.meiner.anselm.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view)
+    public void changetoMap(View view)
     {
         Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    public void changetoHistory(View view)
+    {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void changetoRating(View view)
+    {
+        Intent intent = new Intent(this, RatingActivity.class);
         startActivity(intent);
     }
 
