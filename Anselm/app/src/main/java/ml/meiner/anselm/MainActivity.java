a2890309630caf6f96b602ae67ab141030d49e53
Schimmic <simon.czapski@ewe.net>
2020-01-09 16:13:07 +0200

    Leute, ich bin ANdorid programmier

--- Anselm/app/src/main/java/ml/meiner/anselm/MainActivity.java
+++ Anselm/app/src/main/java/ml/meiner/anselm/MainActivity.java
@@ -2,6 +2,12 @@ package ml.meiner.anselm;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.FirstMap);
 
+    }
+    public class MapsMarkerActivity extends AppCompatActivity
+            implements OnMapReadyCallback {
+        // Include the OnCreate() method here too, as described above.
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            // Add a marker in Sydney, Australia,
+            // and move the map's camera to the same location.
+            LatLng sydney = new LatLng(-33.852, 151.211);
+            googleMap.addMarker(new MarkerOptions().position(sydney)
+                    .title("Marker in Sydney"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+    }
 }
