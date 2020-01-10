5f8710b5de986f9d3555737e7f12d5d3d3101814
T1bolus <33393517+T1bolus@users.noreply.github.com>
2020-01-10 17:57:28 +0100

    Added HistoryList

--- Anselm/app/src/main/java/ml/meiner/anselm/Activties/History.java
+++ Anselm/app/src/main/java/ml/meiner/anselm/Activties/History.java
@@ -1,19 +1,56 @@
 package ml.meiner.anselm.Activties;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import ml.meiner.anselm.R;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import ml.meiner.anselm.R;
+import ml.meiner.anselm.model.historyListDataHandler;
+
 public class History extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private String[] datalist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        datalist = new String[10];
+        datalist[0] = "Auftrag X: Zu viel Geld bekommen und keins abgegeben";
+        datalist[1] = "Irgendein anderer Text der mit der Datenbank aufgefüllt werden muss";
+        datalist[2] = "Irgendein anderer Text der mit der Datenbank aufgefüllt werden muss";
+        datalist[3] = "Irgendein anderer Text der mit der Datenbank aufgefüllt werden muss";
+        datalist[4] = "Irgendein anderer Text der mit der Datenbank aufgefüllt werden muss";
+        datalist[5] = "Irgendein anderer Text der mit der Datenbank aufgefüllt werden muss";
+        datalist[6] = "Irgendein anderer Text der mit der Datenbank aufgefüllt werden muss";
+        datalist[7] = "Irgendein anderer Text der mit der Datenbank aufgefüllt werden muss";
+        datalist[8] = "Irgendein anderer Text der mit der Datenbank aufgefüllt werden muss";
+        datalist[9] = "Irgendein anderer Text der mit der Datenbank aufgefüllt werden muss";
+
+
+
+
+        recyclerView = findViewById(R.id.recycleView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new historyListDataHandler(datalist);
+        recyclerView.setAdapter(mAdapter);
     }
 
     public void gotoMap(View view)
@@ -27,4 +64,8 @@ public class History extends AppCompatActivity {
         Intent intent = new Intent(this, Inseration.class);
         startActivity(intent);
     }
+
+
+
+
 }
---
+++ Anselm/app/src/main/java/ml/meiner/anselm/model/historyListDataHandler.java
@@ -0,0 +1,59 @@
+package ml.meiner.anselm.model;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import ml.meiner.anselm.R;
+
+public class historyListDataHandler extends RecyclerView.Adapter<historyListDataHandler.MyViewHolder> {
+
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textView; //Element to manipulate
+        public MyViewHolder(ConstraintLayout v)  //Layout around XML
+        {
+            super(v);
+            textView = v.findViewById(R.id.textView3);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public historyListDataHandler(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public historyListDataHandler.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        ConstraintLayout v = (ConstraintLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_element, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
