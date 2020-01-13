82638469cba23e33a87b5eba396f8174164ab869
Jan <stueven@uni-bremen.de>
2020-01-13 17:15:39 +0100

    Searchengine eingebaut

--- Anselm/app/src/main/java/ml/meiner/anselm/Activties/Inseration.java
+++ Anselm/app/src/main/java/ml/meiner/anselm/Activties/Inseration.java
@@ -1,21 +1,74 @@
 package ml.meiner.anselm.Activties;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SearchView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.IOException;
+import java.util.List;
 
 import ml.meiner.anselm.R;
 
-public class Inseration extends AppCompatActivity {
+public class Inseration extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+    SupportMapFragment mapFragment;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inseration);
+
+        searchView = findViewById(R.id.sv_location);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.googlemap);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null || !location.equals("")) {
+                   Geocoder geocoder = new Geocoder (Inseration.this);
+                   try {
+                       addressList = geocoder.getFromLocationName(location, 1);
+                   } catch (IOException e) {
+                       e.printStackTrace();
+                   }
+                   Address address = addressList.get(0);
+                    LatLng latlng = new LatLng(address.getLatitude(),address.getLongitude());
+                    map.addMarker(new MarkerOptions().position(latlng).title(location));
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,17));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
     }
 
     public void gotoMap(View view)
