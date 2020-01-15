f8e705bc9b3be3141eb11bf22758230dc23ed0ad
Jan <stueven@uni-bremen.de>
2020-01-15 15:56:17 +0100

    Lalelu, der Button geht gut.

--- Anselm/app/src/main/java/ml/meiner/anselm/Main/MainActivity.java
+++ Anselm/app/src/main/java/ml/meiner/anselm/Main/MainActivity.java
@@ -1,29 +1,83 @@
 package ml.meiner.anselm.Main;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import ml.meiner.anselm.Activties.Inseration;
 import ml.meiner.anselm.Activties.Map;
 import ml.meiner.anselm.Activties.History;
 import ml.meiner.anselm.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+    SupportMapFragment mapFragment;
+
+    Task location;
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.googlemap);
+
+
+
+        mapFragment.getMapAsync(this);
     }
 
-    public void gotoMap(View view)
-    {
-        Intent intent = new Intent(this, Map.class);
-        startActivity(intent);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void gotoInseration(View view)
     {
@@ -34,6 +88,12 @@ public class MainActivity extends AppCompatActivity {
     public void gotoHistory(View view)
     {
         Intent intent = new Intent(this, History.class);
+        startActivity(intent);
+    }
+
+    public void gotoMap(View view)
+    {
+        Intent intent = new Intent(this, Map.class);
         startActivity(intent);
     }
 
