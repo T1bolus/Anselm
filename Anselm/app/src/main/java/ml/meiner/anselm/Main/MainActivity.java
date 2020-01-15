3b04e607668a73d1e32ba95a376252d12dee18db
T1bolus <33393517+T1bolus@users.noreply.github.com>
2020-01-15 17:02:17 +0100

    Login erweitert

--- Anselm/app/src/main/java/ml/meiner/anselm/Main/MainActivity.java
+++ Anselm/app/src/main/java/ml/meiner/anselm/Main/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
@@ -101,8 +102,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             // ...
                             logged_in = false;
 
-                            //Button btn = this.findViewById(R.id.buttonsignup);
-                            //btn.setText("Einloggen");
+                            Button btn = MainActivity.this.findViewById(R.id.buttonsignup);
+                            btn.setText("Einloggen");
+
+                            TextView nameLabel = MainActivity.this.findViewById((R.id.textView));
+                            nameLabel.setText("");
                         }
                     });
         }
@@ -119,7 +123,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 Button btn = this.findViewById(R.id.buttonsignup);
-                //btn.setText("Ausloggen");
+                btn.setText("Ausloggen");
+                TextView nameLabel = this.findViewById((R.id.textView));
+                nameLabel.setText("Hallo " + user.getDisplayName());
                 logged_in = true;
 
                 // ...
