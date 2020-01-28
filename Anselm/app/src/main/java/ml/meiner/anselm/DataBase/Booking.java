6fccf0090daf43d8a6503edab98d9048dfd5f3c0
T1bolus <33393517+T1bolus@users.noreply.github.com>
2020-01-28 17:58:28 +0100

    fixed evtl wieder login

---
+++ Anselm/app/src/main/java/ml/meiner/anselm/DataBase/Booking.java
@@ -0,0 +1,17 @@
+package ml.meiner.anselm.DataBase;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Booking
+{
+    private String id;
+    private Chargingstation station;
+    private String username;
+    private String usernamePicturePath;
+    private String uid;
+
+    ArrayList<Date> bookedTimes;
+
+
+}
