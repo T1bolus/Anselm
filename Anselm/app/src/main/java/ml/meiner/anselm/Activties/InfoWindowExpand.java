31f8fbc17f0fcb3480178db8390899104b9dde76
T1bolus <33393517+T1bolus@users.noreply.github.com>
2020-02-19 16:20:19 +0100

    added logic to booking

--- Anselm/app/src/main/java/ml/meiner/anselm/Activties/InfoWindowExpand.java
+++ Anselm/app/src/main/java/ml/meiner/anselm/Activties/InfoWindowExpand.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -16,8 +18,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 import androidx.appcompat.app.AppCompatActivity;
 import ml.meiner.anselm.DataBase.Booking;
@@ -93,6 +98,11 @@ public class InfoWindowExpand extends AppCompatActivity {
             // TextView freeView = findViewById(R.id.freeTimesView);
             //freeView.setText(freeString);
 
+            Spinner dropdown = findViewById(R.id.planets_spinner3);
+            String[] items = new String[]{"Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"};
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+            dropdown.setAdapter(adapter);
+
         } else {
             finish();
             return;
@@ -143,6 +153,42 @@ public class InfoWindowExpand extends AppCompatActivity {
 
     }
 
+    public static Date getWeekStartDate() {
+        Calendar calendar = Calendar.getInstance();
+        while (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+            calendar.add(Calendar.DATE, -1);
+        }
+        return calendar.getTime();
+    }
+
+    public int giveDay(String day){
+        int m_day=99;
+        switch (day) {
+            case ("Mo"):
+                m_day=0;
+                return m_day;
+            case ("Di"):
+                m_day=1;
+                return m_day;
+            case ("Mi"):
+                m_day=2;
+                return m_day;
+            case ("Do"):
+                m_day=3;
+                return m_day;
+            case ("Fr"):
+                m_day=4;
+                return m_day;
+            case ("Sa"):
+                m_day=5;
+                return m_day;
+            case ("So"):
+                m_day=6;
+                return m_day;
+        }
+        return m_day;
+    }
+
     public void bookAStation(View view) {
 
         if (user == null) {
@@ -162,13 +208,47 @@ public class InfoWindowExpand extends AppCompatActivity {
         book.setUid(user.getUid());
         book.setUsernamePicturePath(user.getPhotoUrl().toString());
 
+
+
+        Spinner spinner = findViewById(R.id.planets_spinner3);
+        int days = giveDay(spinner.getSelectedItem().toString());
+
+
+
+        Date weekStart = getWeekStartDate();
+        Date startDay = weekStart;
+
+        //Increment startDay
+        Calendar calStart = Calendar.getInstance();
+        calStart.setTime(startDay);
+        calStart.add(Calendar.DATE, days);
+
+        String giveDateFrom = calStart.get(Calendar.YEAR) + "-" + (calStart.get(Calendar.MONTH)+1) + "-" + calStart.get(Calendar.DAY_OF_MONTH);
+        String giveTimeFrom = selected_hourFrom + ":" + selected_minuteFrom;
+        String giveTimeTo = selected_minuteFrom + ":" + selected_minuteTo;
+        Date realDateFrom = null;
+        Date realDateTo = null;
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        try {
+            realDateFrom = formatter.parse(giveDateFrom + " " + giveTimeFrom);
+            realDateTo = formatter.parse(giveDateFrom + " " + giveTimeTo);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         if((selected_hourTo < selected_hourFrom) || (selected_hourTo == selected_hourFrom && selected_minuteTo < selected_minuteFrom) || (selected_hourTo == 0 && selected_hourFrom == 0 && selected_minuteTo == 0 && selected_minuteFrom == 0))
         {
             Toast.makeText(this, "Impossible Time!", Toast.LENGTH_LONG).show();
             return;
         }
 
-        
+        ArrayList<Date> bookedtimes = new ArrayList<>();
+        bookedtimes.add(realDateFrom);
+        bookedtimes.add(realDateTo);
+        book.setBookedTimes(bookedtimes);
+
+
 
         FirestoreDatabase.getInstance().addBooking(this,book);
 
